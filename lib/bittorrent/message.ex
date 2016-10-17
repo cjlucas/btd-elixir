2b30ecc59e33ed06da436eddb3adf382b22523c2
Chris Lucas <chris@chrisjlucas.com>
2016-10-16 21:08:20 -0700

    Cleaned up code

--- lib/bittorrent/message.ex
+++ lib/bittorrent/message.ex
@@ -4,41 +4,50 @@ defmodule Bittorrent.Message do
   end
 
   defmodule Request do
-    defstruct id: 6, index: 0, begin: 0, length: 0
+    defstruct index: 0, begin: 0, length: 0
   end
 
   defmodule Piece do
-    defstruct id: 7, index: 0, begin: 0, block: <<>>
+    defstruct index: 0, begin: 0, block: <<>>
   end
+
+  @message_registry [
+    {:choke, 1},
+    {:request, 6},
+    {:piece, 7}
+  ]
 
   def parse(<< id, payload :: binary >>) do
-    case id do
-      1 -> {:ok, %Choke{}}
-      6 -> decode(%Request{}, payload)
-      7 -> decode(%Piece{}, payload)
-      _ -> {:error, :unknown_id}
-    end
+    List.keyfind(@message_registry, id, 1)
+    |> elem(0)
+    |> decode(payload)
   end
 
-  defp decode(msg = %Request{}, payload) do
+  defp decode(:choke, _), do: {:ok, %Choke{}}
+
+  defp decode(:request, payload) do
     case payload do
       <<
         index :: big-size(32),
         begin :: big-size(32), 
         len :: big-size(32)
-      >> -> {:ok, %{msg | index: index, begin: begin, length: len}}
+      >> -> {:ok, %Request{index: index, begin: begin, length: len}}
       _ -> {:error, :decode_failed}
     end
   end
 
-  defp decode(msg = %Piece{}, payload) do
+  defp decode(:piece, payload) do
     case payload do
       <<
         index :: big-size(32),
         begin :: big-size(32), 
         block :: binary
-      >> -> {:ok, %{msg | index: index, begin: begin, block: block}}
+      >> -> {:ok, %Piece{index: index, begin: begin, block: block}}
       _ -> {:error, :decode_failed}
     end
+  end
+
+  defp decode(_, _payload) do
+    {:error, :unknown_id}
   end
 end
