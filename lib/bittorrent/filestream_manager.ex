e9cea99c4e577a64800135bb81eefb983c4e421f
Chris Lucas <chris@chrisjlucas.com>
2016-10-24 16:34:02 -0700

    Added block writing support

--- lib/bittorrent/filestream_manager.ex
+++ lib/bittorrent/filestream_manager.ex
@@ -26,25 +26,26 @@ defmodule FileStream.Manager do
     GenServer.call(pid, {:read_block, piece_idx, offset, length})
   end
 
-  def write_block(pid, piece_idx, offset, length, data) do
-    GenServer.cast(pid, {:write_block, piece_idx, offset, length, data})
+  def write_block(pid, piece_idx, offset, data) do
+    GenServer.cast(pid, {:write_block, piece_idx, offset, data})
   end
 
-  def chunk([], _, acc), do: Enum.reverse(acc)
-  def chunk([n | sizes], buf, acc) do
+  defp chunk(sizes, buf), do: chunk(sizes, buf, [])
+  defp chunk([], _, acc), do: Enum.reverse(acc)
+  defp chunk([n | sizes], buf, acc) do
     << buf :: bytes-size(n), rest :: binary >> = buf
     chunk(sizes, rest, [buf | acc])
   end
 
-  defp write(fpath, off, len) do
-    with {:ok, f} = :file.open(fpath, [:write, :binary]),
-         {:ok, data} = :file.pwrite(f, off, len),
+  defp write(fpath, off, data) do
+    with {:ok, f} = :file.open(fpath, [:read, :write, :binary, :raw]),
+         :ok = :file.pwrite(f, off, data),
          :ok = :file.close(f),
-      do: data 
+      do: :ok
   end
 
   defp read(fpath, off, len) do
-    with {:ok, f} = :file.open(fpath, [:read, :binary]),
+    with {:ok, f} = :file.open(fpath, [:read, :binary, :raw]),
          {:ok, data} = :file.pread(f, off, len),
          :ok = :file.close(f),
       do: data 
@@ -59,5 +60,19 @@ defmodule FileStream.Manager do
            |> Enum.reduce(fn buf, acc -> acc <> buf end)
 
     {:reply, data, state}
+  end
+
+  def handle_cast({:write_block, piece_idx, offset, data}, state) do
+    offset = piece_idx * state.piece_size + offset
+    segments = Bittorrent.FileStream.items_in_range(state.files, offset, byte_size(data))
+
+    segments
+    |> Enum.map(&(&1.size))
+    |> chunk(data)
+    |> Enum.zip(segments)
+    |> Enum.map(fn t -> Map.merge(elem(t, 1), %{data: elem(t, 0)}) end)
+    |> Enum.each(fn seg -> write(Path.join(state.root, seg.name), seg.offset, seg.data) end)
+
+    {:noreply, state}
   end
 end
