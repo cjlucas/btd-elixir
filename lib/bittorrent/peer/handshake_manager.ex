9229a592f9c7e4bd3f8d3243c0e085fca42bb255
Chris Lucas <chris@chrisjlucas.com>
2016-11-28 16:50:04 -0700

    HandshakeManager: Stuff

--- lib/bittorrent/peer/handshake_manager.ex
+++ lib/bittorrent/peer/handshake_manager.ex
@@ -2,23 +2,57 @@ defmodule Peer.Handshake do
   @p 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A63A36210000000000090563
 
   def gen_keys do
-    privkey = :crypto.strong_rand_bytes(:crypto.rand_uniform(161)-1)
+    privkey = :crypto.strong_rand_bytes(rand(160))
     pubkey = :crypto.mod_pow(2, privkey, @p)
     {privkey, pubkey}
   end
 
   def calc_secret(pubkey, privkey), do: :crypto.mod_pow(pubkey, privkey, @p)
 
+  def init_streams(in_key, out_key) do
+    [ins, outs] = [in_key, out_key]
+                  |> Enum.map(&(:crypto.stream_init(:rc4, &1)))
+                  |> Enum.map(&(:crypto.stream_decrypt(&1, :crypto.strong_rand_bytes(1024))))
+                  |> Enum.map(&(elem(&1, 0)))
+    {ins, outs}
+  end
+
   def req1(s), do: hash([<<"req1">>, s])
 
   def req2(skey), do: hash([<<"req2">>, skey])
   
   def req3(s), do: hash([<<"req3">>, s])
+
+  def key(prefix, s, skey), do: hash([prefix, s, skey])
   
-  defp hash(buf), do: :crypto.hash(:sha, buf)
+  def hash(buf), do: :crypto.hash(:sha, buf)
+  
+  def rand(n), do: :rand.uniform(n+1) - 1
+
+  def sync(_needle, <<>>), do: <<>>
+  def sync(needle, haystack) do
+    size = byte_size(needle)
+    case haystack do
+      << ^needle::bytes-size(size), rest::binary >> ->
+        rest
+      << _::8, rest::binary >> ->
+        sync(needle, rest)
+    end
+  end
+
+  def bin_xor(bin1, bin2) do
+    use Bitwise
+
+    [l1, l2] = Enum.map([bin1, bin2], &:erlang.binary_to_list/1)
+    Enum.zip(l1, l2)
+    |> Enum.map(fn e -> elem(e, 0) ^^^ elem(e, 1) end)
+    |> :erlang.list_to_binary
+  end
 end
 
 defmodule Peer.HandshakeManager do
+  import Peer.Handshake
+  import :erlang, only: [iolist_size: 1, iolist_to_binary: 1]
   use GenServer
   require Logger
 
@@ -28,7 +62,7 @@ defmodule Peer.HandshakeManager do
   @crypto_provide <<2::32>> # rc4 only
 
   @pstr "BitTorrent protocol"
-  @pstrlen <<19>>
+  @pstrlen 19
 
   # TODO: this should be in external config
   @sock_timeout 1000
@@ -51,6 +85,12 @@ defmodule Peer.HandshakeManager do
       end
     end
 
+    def recv(%Connection{sock: sock} = conn, len, timeout \\ 1000) do
+      with {:ok, data} <- :gen_tcp.recv(sock, len, timeout) do
+        {:ok, decrypt(conn, data)}
+      end
+    end
+
     def decrypt(%Connection{in_stream: stream} = conn, data) do
       {stream, data} = :crypto.stream_decrypt(stream, data)
       {%{conn | in_stream: stream}, data}
@@ -68,7 +108,7 @@ defmodule Peer.HandshakeManager do
               privkey: <<>>, # our privkey
               secret: <<>>,  # S
               crypto_select: nil,
-              expected_vc: nil
+              expected_vc: nil # the encrypted vc to sync on
   end
 
   defmodule State do
@@ -102,7 +142,6 @@ defmodule Peer.HandshakeManager do
 
     def register_peer(%State{peers: peers, buffers: buffers} = state, direction, sock, info_hash \\ <<>>) do
       flow = if direction == :in, do: @incoming_flow, else: @outgoing_flow
-      IO.puts("OMGHERE #{inspect flow}")
       peers = Map.put(peers, sock, %HandshakeInfo{
                       incoming: direction == :in,
                       info_hash: info_hash,
@@ -139,6 +178,10 @@ defmodule Peer.HandshakeManager do
     end
   end
 
+  def peer_info(sock) do
+    GenServer.call(@name, {:peer_info, sock})
+  end
+
   defp trigger_handler(sock), do: GenServer.cast(@name, {:trigger_handler, sock})
 
   def handle_call({:register_outgoing_peer, sock, info_hash}, _from, state) do
@@ -151,12 +194,17 @@ defmodule Peer.HandshakeManager do
     {:reply, :ok, State.register_peer(state, :in, sock)}
   end
 
+  def handle_call({:peer_info, sock}, _from, state) do
+    IO.puts(inspect state.peers[sock])
+    {:reply, state.peers[sock], state}
+  end
+
   def handle_cast({:trigger_handler, sock}, state), do: dispatch_handler(state.peers[sock], state)
 
   def handle_info({:tcp, sock, data}, %State{peers: peers} = state) do
-    GenServer.cast(@name, {:trigger_handler, sock})
+    trigger_handler(sock)
+    Logger.info("GOT SOME DATA info = #{inspect peers[sock]}")
     peers = update_in(peers[sock].buffer, &([&1, data]))
-    IO.puts("OMGHERE #{inspect peers}")
     {:noreply, %{state | peers: peers}}
   end
 
@@ -168,23 +216,28 @@ defmodule Peer.HandshakeManager do
   
   defp dispatch_handler(info, state) when is_nil(info), do: {:noreply, state}
   defp dispatch_handler(%{conn: conn, states: [cur_state | rem_states]} = info, state) do
+    Logger.debug("cur_state = #{cur_state}")
     case handle_state(cur_state, info, conn) do
       {:next_state, info} ->
+        Logger.debug("Got :next_state, states now: #{inspect rem_states}")
         info = %{info | states: rem_states}
-        IO.puts("Got :next_state, states now: #{inspect info.states}")
-        trigger_handler(conn) # queue the next state handler
-        {:noreply, put_in(state.peers[conn], info)}
+        trigger_handler(conn.sock) # queue the next state handler
+        {:noreply, put_in(state.peers[conn.sock], info)}
       :no_change ->
+        Logger.debug("no_change state = #{inspect state}")
         {:noreply, state}
       {:error, reason} ->
         # TODO: proper error handling
-      IO.puts("Handler for state #{state} returned an error with reason #{reason}")
+        IO.puts("Handler for state returned an error with reason #{reason}")
+        {:noreply, state}
     end
   end
   
   defp handle_state(:send_pubkey, info, conn) do
-    {privkey, pubkey} = Peer.Handshake.gen_keys()
+    {privkey, pubkey} = gen_keys()
     pad = :crypto.strong_rand_bytes(rand(512))
+
+    Logger.debug("omgwtf pubkey = #{inspect pubkey}")
 
     case Connection.send(conn, [pubkey, pad]) do
       {:ok, conn} ->
@@ -194,23 +247,24 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:recv_pubkey, %HandshakeInfo{buffer: buf} = info, _conn) do
-    IO.puts("GOT DATA IN THIS SHIT #{inspect buf}")
-    if :erlang.iolist_size(buf) >= 96 do
-      << pubkey :: bytes-size(96), rest :: binary >> = :erlang.iolist_to_binary(buf)
-      IO.puts("set pubkey in info #{inspect pubkey}")
+  defp handle_state(:recv_pubkey, %{buffer: buf} = info, _conn) do
+    Logger.debug("recv_pubkey, byte_size(buf) = #{iolist_size(buf)}")
+    if iolist_size(buf) >= 96 do
+      << pubkey :: bytes-size(96), rest :: binary >> = iolist_to_binary(buf)
+      Logger.debug("got pubkey #{inspect pubkey}")
       {:next_state, %{info | pubkey: pubkey, buffer: [rest]}}
     else
+      Logger.debug("DUDE HERE")
       :no_change
     end
   end
   
-  defp handle_state(:calc_secret, %HandshakeInfo{pubkey: pub, privkey: priv} = info, _conn) do
-    IO.puts("S = #{inspect :crypto.mod_pow(pub, priv, @p)}")
-    {:next_state, %{info | secret: :crypto.mod_pow(pub, priv, @p)}}
+  defp handle_state(:calc_secret, %{pubkey: pub, privkey: priv} = info, _conn) do
+    Logger.debug("S = #{inspect calc_secret(pub, priv)}")
+    {:next_state, %{info | secret: calc_secret(pub, priv)}}
   end
 
-  defp handle_state(:send_reqs, %HandshakeInfo{secret: s, info_hash: info_hash} = info, conn) do
+  defp handle_state(:send_reqs, %{secret: s, info_hash: info_hash} = info, conn) do
     payload = [
       hash([<<"req1">>, s]),
       bin_xor(hash([<<"req2">>, info_hash]), hash([<<"req3">>, s]))
@@ -224,18 +278,18 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:recv_reqs, %HandshakeInfo{secret: s, buffer: buf} = info, _conn) do
-    if :erlang.iolist_size(buf) >= 40 do
+  defp handle_state(:recv_reqs, %{secret: s, buffer: buf} = info, _conn) do
+    if iolist_size(buf) >= 40 do
       <<
-        req1 :: bytes-size(20),
-        req23 :: bytes-size(20),
+        req1buf :: bytes-size(20),
+        req23buf :: bytes-size(20),
         rest :: binary
-      >> = :erlang.iolist_to_binary(buf)
+      >> = iolist_to_binary(buf)
 
       # TODO: extract lookup req2 in torrent store
 
-      case <<"req1">> <> s do
-        ^req1 ->
+      case req1(s) do
+        ^req1buf ->
           {:next_state, %{info | buffer: [rest]}}
         _ ->
           {:error, :bad_req1}
@@ -245,21 +299,13 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:init_streams, %HandshakeInfo{incoming: true, secret: s, info_hash: hash} = info, conn) do
-    ins = :crypto.stream_init(:rc4, hash(<<"keyA">> <> s <> hash))
-    outs = :crypto.stream_init(:rc4, hash(<<"keyB">> <> s <> hash))
-    {ins, _} = :crypto.stream_decrypt(ins, :crypto.strong_rand_bytes(1024))
-    {outs, _} = :crypto.stream_encrypt(outs, :crypto.strong_rand_bytes(1024))
-
+  defp handle_state(:init_streams, %{incoming: true, secret: s, info_hash: hash} = info, conn) do
+    {ins, outs} = init_streams(key(<<"keyA">>, s, hash), key(<<"keyB">>, s, hash))
     {:next_state, put_in(info.conn,  %{conn | in_stream: ins, out_stream: outs})}
   end
   
-  defp handle_state(:init_streams, %HandshakeInfo{incoming: false, secret: s, info_hash: hash} = info, conn) do
-    ins = :crypto.stream_init(:rc4, hash(<<"keyB">> <> s <> hash))
-    outs = :crypto.stream_init(:rc4, hash(<<"keyA">> <> s <> hash))
-    {ins, _} = :crypto.stream_decrypt(ins, :crypto.strong_rand_bytes(1024))
-    {outs, _} = :crypto.stream_encrypt(outs, :crypto.strong_rand_bytes(1024))
-
+  defp handle_state(:init_streams, %{incoming: false, secret: s, info_hash: hash} = info, conn) do
+    {ins, outs} = init_streams(key(<<"keyB">>, s, hash), key(<<"keyA">>, s, hash))
     {:next_state, put_in(info.conn,  %{conn | in_stream: ins, out_stream: outs})}
   end
 
@@ -272,17 +318,17 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:recv_vc, %HandshakeInfo{expected_vc: vc} = info, conn) when is_nil(vc) do
+  defp handle_state(:recv_vc, %{expected_vc: vc} = info, conn) when is_nil(vc) do
     {stream, vc} = :crypto.stream_encrypt(conn.in_stream, @vc)
     conn = %{conn | in_stream: stream}
     handle_state(:recv_vc, %{info | expected_vc: vc, conn: conn}, conn)
   end
   
-  defp handle_state(:recv_vc, %HandshakeInfo{expected_vc: vc, buffer: buf} = info, _conn) do
-    if :erlang.iolist_size(buf) >= byte_size(vc) do
-      rest = sync(vc, :erlang.iolist_to_binary(buf))
+  defp handle_state(:recv_vc, %{expected_vc: vc, buffer: buf} = info, _conn) do
+    if iolist_size(buf) >= byte_size(vc) do
+      rest = sync(vc, iolist_to_binary(buf))
       if byte_size(rest) > 0 do
-        {:next_state, %{info | buffer: rest}}
+        {:next_state, %{info | buffer: [rest]}}
       else
         :no_change
       end
@@ -291,9 +337,9 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:recv_crypto_select, %HandshakeInfo{buffer: buf} = info, conn) do
-    if :erlang.iolist_size(buf) >= byte_size(@crypto_provide) do
-      << v::32, rest::binary >> = :erlang.iolist_to_binary(buf)
+  defp handle_state(:recv_crypto_select, %{buffer: buf} = info, conn) do
+    if iolist_size(buf) >= byte_size(@crypto_provide) do
+      << v::32, rest::binary >> = iolist_to_binary(buf)
       {:next_state, %{info | crypto_select: v, buffer: [rest]}}
     else
       :no_change
@@ -309,11 +355,11 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:recv_pad, %HandshakeInfo{buffer: buf}, _conn) when byte_size(buf) < 2 do
+  defp handle_state(:recv_pad, %{buffer: buf}, _conn) when byte_size(buf) < 2 do
     :no_change
   end
   
-  defp handle_state(:recv_pad, %HandshakeInfo{buffer: buf} = info, _conn) do
+  defp handle_state(:recv_pad, %{buffer: buf} = info, _conn) do
     << len::16, rest::binary >> = buf
     case rest do
       << _::bytes-size(len), rest::binary>> ->
@@ -343,11 +389,11 @@ defmodule Peer.HandshakeManager do
     end
   end
 
-  defp handle_state(:send_handshake, %HandshakeInfo{info_hash: info_hash} = info, conn) do
+  defp handle_state(:send_handshake, %{info_hash: info_hash} = info, conn) do
     case Torrent.Store.lookup(:info_hash, info_hash) do
       {:ok, t} ->
         payload = [
-          @pstrlen,
+          <<@pstrlen::8>>,
           @pstr,
           <<0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00>>,
           info_hash,
@@ -366,29 +412,5 @@ defmodule Peer.HandshakeManager do
 
   defp handle_state(state, _info, _conn) do
     raise "Unhandled state #{state}"
-  end
-
-  defp rand(n), do: :rand.uniform(n+1) - 1
-  
-  defp hash(buf), do: :crypto.hash(:sha, buf)
-
-  defp bin_xor(bin1, bin2) do
-    use Bitwise
-
-    [l1, l2] = Enum.map([bin1, bin2], &:erlang.binary_to_list/1)
-    Enum.zip(l1, l2)
-    |> Enum.map(fn e -> elem(e, 0) ^^^ elem(e, 1) end)
-    |> :erlang.list_to_binary
-  end
-
-  def sync(_needle, <<>>), do: <<>>
-  def sync(needle, haystack) do
-    size = byte_size(needle)
-    case haystack do
-      << ^needle::bytes-size(size), rest::binary >> ->
-        rest
-      << _::8, rest::binary >> ->
-        sync(needle, rest)
-    end
   end
 end
--- test/bittorrent/peer/handshake_manager_test.exs
+++ test/bittorrent/peer/handshake_manager_test.exs
@@ -1,3 +1,20 @@
+defmodule MockTorrentStore do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: Torrent.Store)
+  end
+
+  def lookup(:info_hash, info_hash) do
+    IO.puts("OMGWAS CALLED")
+    {:ok, %Torrent{peer_id: <<0::20*8>>}}
+  end
+
+  def init(:ok) do
+    {:ok, nil}
+  end
+end
+
 defmodule Peer.HandshakeManagerTest do
   use ExUnit.Case
 
@@ -19,14 +36,46 @@ defmodule Peer.HandshakeManagerTest do
   end
 
   test "outgoing connection", ctx do
-    :ok = Peer.HandshakeManager.start_link
-    :ok = Torrent.Store.start_link
+    {:ok, _} = Peer.HandshakeManager.start_link
+    {:ok, _} = MockTorrentStore.start_link
 
     {:ok, port} = :inet.port(ctx.listen)
     {:ok, csock} = :gen_tcp.connect({127,0,0,1}, port, [:binary, active: true])
     {:ok, ssock} = :gen_tcp.accept(ctx.listen)
 
+    assert Peer.HandshakeManager.register_outgoing_peer(csock, @info_hash) == :ok
 
-    assert :ok == Peer.HandshakeManager.register_outgoing_peer(csock, @info_hash)
+    {:ok, << pubA::bytes-size(96), data::binary>>} = :gen_tcp.recv(ssock, 0)
+    IO.puts("pubA = #{inspect pubA}")
+    
+    {priv, pub} = Peer.Handshake.gen_keys()
+    IO.puts("OMGHERE #{inspect pub}")
+    :ok = :gen_tcp.send(ssock, [pub, <<0::16>>])
+
+    s = Peer.Handshake.calc_secret(pubA, priv)
+    IO.puts("DA S = #{inspect s}")
+
+    data = with {:ok, buf} <- :gen_tcp.recv(ssock, 0), do: data <> buf
+    IO.puts("got data #{byte_size(data)}")
+    rest = Peer.Handshake.sync(Peer.Handshake.req1(s), data)
+    assert byte_size(rest) > 0
+    data = rest
+
+    << req23::bytes-size(20), data::binary >> = data
+    req2 = Peer.Handshake.bin_xor(Peer.Handshake.req3(s), req23)
+    assert Peer.Handshake.req2(@info_hash) == req2
+
+    {ins, outs} = Peer.Handshake.init_streams(
+      Peer.Handshake.key(<<"keyA">>, s, @info_hash),
+      Peer.Handshake.key(<<"keyB">>, s, @info_hash)
+    )
+
+    conn = %Peer.HandshakeManager.Connection{in_stream: ins, out_stream: outs, sock: ssock}
+    {:ok, {conn, << vc::bytes-size(8), crypto_provide::32, lenpad::8, data::binary>>}} = Peer.HandshakeManager.Connection.recv(conn, 0)
+    assert vc == <<0::64>>
+
+    <<pad::bytes-size(lenpad), data::binary>> = data
+    <<pstrlen::8, data::binary>> = data
+    assert pstrlen == 19
   end
 end
