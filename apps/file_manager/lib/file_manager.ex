ac1db67d0ac15355eae6921fe556436c263e4604
Chris Lucas <chris@chrisjlucas.com>
2017-01-22 18:49:10 -0700

    Add File.Manager.Supervisor; Initial public API

--- apps/file_manager/lib/file_manager.ex
+++ apps/file_manager/lib/file_manager.ex
@@ -3,10 +3,13 @@ defmodule FileManager do
   Documentation for FileManager.
   """
 
+  @block_size 16384
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
+      supervisor(File.Manager.Supervisor, []),
       supervisor(Registry, [:unique, File.Manager.Registry], id: File.Manager.Registry),
       supervisor(Torrent.FileHandler.Supervisor, []),
       worker(File.Manager.Store, []),
@@ -15,5 +18,25 @@ defmodule FileManager do
     ]
 
     Supervisor.start_link(children, [strategy: :one_for_one])
+  end
+
+  def register(info_hash, root, files, piece_hashes, piece_size) do
+    :ok = File.Manager.Store.add(info_hash, root, files, piece_hashes, piece_size, @block_size)
+    {:ok, _} = File.Manager.Supervisor.start_child(info_hash)
+    :ok
+  end
+
+  def deregister(info_hash) do
+    with :ok <- File.Manager.Store.remove(info_hash),
+         :ok <- Supervisor.terminate_child(File.Manager.Supervisor, info_hash),
+         :ok <- Supervisor.delete_child(File.Manager.Supervisor, info_hash), do: :ok
+  end
+
+  def write_block(info_hash, piece_idx, offset, data) do
+    File.Manager.write_block(info_hash, piece_idx, offset, data)
+  end
+
+  def read_block(info_hash, piece_idx, offset, size) do
+    File.Manager.read_block(info_hash, piece_idx, offset, size)
   end
 end
