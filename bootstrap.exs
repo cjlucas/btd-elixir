e992689d709bab5dd263db848a0e188fbb77ad19
Chris Lucas <chris@chrisjlucas.com>
2017-01-27 15:24:24 -0700

    Use app API in bootstrap script

--- bootstrap.exs
+++ bootstrap.exs
@@ -1,30 +1,13 @@
 #:observer.start
 #Process.sleep(5_000)
 
-info_hash = <<4, 3, 251, 71, 40, 189, 120, 143, 188, 182, 126, 135, 214, 254, 178, 65, 239, 56, 199, 90>>
-url = "http://torrent.ubuntu.com:6969/announce"
-
 {:ok, torrent} = File.read!("apps/torrent_manager/test/fixtures/ubuntu-16.10-desktop-amd64.iso.torrent")
                  |> Torrent.parse
 
-IO.puts("num pieces: #{length(torrent.pieces)}")
-
-{:ok, _} = Torrent.Registry.register(torrent)
-
-files = torrent.files |> Enum.map(fn %{path: path, size: size} -> {path, size} end)
-:ok = FileManager.register(info_hash,
-                           "/Users/chris/Downloads",
-                           files,
-                           torrent.pieces,
-                           torrent.piece_length)
-
-Peer.Manager.Store.add(info_hash)
-{:ok, _} = Peer.Manager.Supervisor.start_child(info_hash)
+:ok = TorrentManager.register(torrent, "/Users/chris/Downloads")
+:ok = TorrentManager.start(torrent.info_hash)
 
 TrackerManager.subscribe(:received_response)
-:ok = Tracker.Manager.register(info_hash, info_hash, [[url]])
-:ok = Tracker.Manager.request(info_hash, :started, 0, 0, 0)
-
 
 receive do
   {:received_response, info_hash, url, resp} ->
